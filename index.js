ecb70d8f7d57f18e004f1d6339c38fb3d985ab09
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-03-30 23:50:14 +0530

    update

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const { s3Uploadv2 } = require("./s3Service")
 require('dotenv').config()
 const app = express()
 
-const corsOptions = {
-  origin: 'https://personal-d77263590-alicehack2020.vercel.app',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+ 
 
-app.use(cors(corsOptions))
+app.use(cors())
 
 const storage=multer.memoryStorage()
 const PORT=process.env.PORT
@@ -20,8 +17,7 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   try {
     const file = req.file
     const result = await s3Uploadv2(file)
-    res.set("Access-Control-Allow-Origin", "https://personal-d77263590-alicehack2020.vercel.app")
-    res.json({ status: "success", data: result.Location }) 
+     res.json({ status: "success", data: result.Location }) 
   } catch (error) {
     console.log(error)  
   }
