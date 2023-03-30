ef1b64810c93a1a6e0041bb0c3657ff2529729ea
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-03-30 23:32:37 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const express=require("express")
 const { s3Uploadv2 } = require("./s3Service")
 require('dotenv').config()
 const app = express()
+
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const storage=multer.memoryStorage()
 const PORT=process.env.PORT
