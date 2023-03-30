33686a2a2b77e4a8feffcd598fd890169e019947
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-03-30 23:36:14 +0530

    update

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const app = express()
 
 app.use(cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 const storage=multer.memoryStorage()
 const PORT=process.env.PORT
 const upload = multer({ storage })
