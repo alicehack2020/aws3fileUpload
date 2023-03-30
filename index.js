12e842f96891208369331ae7815844d0566578a3
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-03-30 23:41:46 +0530

    update

--- index.js
+++ index.js
@@ -6,21 +6,25 @@ const { s3Uploadv2 } = require("./s3Service")
 require('dotenv').config()
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://personal-d77263590-alicehack2020.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 const storage=multer.memoryStorage()
 const PORT=process.env.PORT
 const upload = multer({ storage })
-app.post("/upload",upload.single("file"),async(req, res) => {
-    try {
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
     const file = req.file
     const result = await s3Uploadv2(file)
-    res.json({ status: "sucess",data:result.Location}) 
-    }
-    catch (error) {
-      console.log(error)  
-    }
-   
+    res.set("Access-Control-Allow-Origin", "https://personal-d77263590-alicehack2020.vercel.app")
+    res.json({ status: "success", data: result.Location }) 
+  } catch (error) {
+    console.log(error)  
+  }
 })
 app.get("/", (req, res) => {
   res.send({data:"welcome"})
